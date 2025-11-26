2579f2d209f903f400103dc1319eee6c0d9f2a1e
davchztr <davindra.chesta@gmail.com>
2025-11-26 23:49:37 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../ui/ui_splash.dart';
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        textTheme: GoogleFonts.albertSansTextTheme(),
         useMaterial3: true,
       ),
       home: const SplashPage(), 
